96c9bbb49b652011cba18ede549ec93b3cb5e5aa
venkat charan <srvenkatcharan@gmail.com>
2026-06-04 12:59:55 +0530

    Add files via upload

---
+++ FoodOrdering.Store/Abstractions/IUserStore.cs
@@ -0,0 +1,9 @@
+﻿using FoodOrdering.Common.Models;
+
+namespace FoodOrdering.Store.Abstractions
+{
+    public interface IUserStore
+    {
+        Task<User> GetUserByEmail(string email);
+    }
+}
